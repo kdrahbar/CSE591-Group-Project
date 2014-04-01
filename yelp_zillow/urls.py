8380b72d583e7255ccd3de5cb8ecec897852ed99
hdamania <harshdamania@gmail.com>
2014-04-01 14:43:53 -0700

    Revert 7dae9bb..7bddcea
    This rolls back to commit 7dae9bb12c387566f3c67b63a85a61f84d019857.

---
+++ yelp_zillow/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from yelp_zillow.views import home
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.conf import settings
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/$'			   , home),
+    url(r'^$'			   , home),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
+urlpatterns += staticfiles_urlpatterns()
---
+++ yelp_zillow/views.py
@@ -0,0 +1,8 @@
+import mysql.connector
+from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+from django.template import Template
+
+def home(request):
+    return render(request, 'bare.html')
