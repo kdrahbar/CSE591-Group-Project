7bddceafd7722c77b5258839570a69d302e6ea44
kdrahbar <kdrahbar@asu.edu>
2014-04-01 14:34:25 -0700

    removed random files

--- yelp_zillow/views.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-from django.shortcuts import render
-import datetime
-from django.http import HttpResponse
-from django.template import Template
-
-def home(request):
-    return render(request, 'bare.html')
