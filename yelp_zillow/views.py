b67e2b6d7e423bd71f9f58a94a40b7d4e0528187
kdrahbar <kdrahbar@asu.edu>
2014-04-14 15:43:07 -0700

    comit

--- yelp_zillow/views.py
+++ yelp_zillow/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 import datetime
 from django.http import HttpResponse
 from django.template import Template
+from sprocs import yelp_table, zillow_table
 
 def home(request):
     return render(request, 'bare.html')
+
+def search(request):
+	if request.POST:
+		city = str(request.POST.get('city'))
+		restaurants, ratings = yelp_table.get_restaurants(city)
+		sMale, sFemale, homPrice, singleHome, rentPer, averageIncome = zillow_table.get_data(city)
+		
--- zillow_api.py
+++ zillow_api.py
@@ -2,50 +2,129 @@ import requests
 from xml.etree import ElementTree
 import urllib2
 from xml.dom.minidom import parseString
+import xml.etree.ElementTree as ET
+import mysql.connector
 
-def original_dom(data):
-	dom = parseString(data)
-	xmlTag = dom.getElementsByTagName('response')[0].toxml()
-	xmlData=xmlTag.replace('<response>','').replace('</response>','')
-	return xmlData
+DB_NAME = 'yelp_zillow'
 
-URL = 'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Tempe'
-#import easy to use xml parser called minidom:
-#from xml.dom.minidom import parseString
-#r = requests.get('http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Tempe')
-#tree = ElementTree.fromstring(r.content)
+URLS = ['http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Tempe',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Phoenix',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Scottsdale',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Chandler',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Laveen',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Goodyear',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Buckeye',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Glendale',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Peoria',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Gilbert',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Mesa',
+	   'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1dq74ir953f_8fetn&state=AZ&city=Tolleson']
+CITIES = ['Tempe', 'Phoenix', 'Scottsdale', 'Chandler', 'Laveen', 'Goodyear', 'Buckeye', 'Glendale', 'Peoria', 'Gilbert', 'Mesa', 'Tolleson']
 
 tags_for_Median_Home_val = ['pages', 'page', 'tables', 'table', 'data', 'attribute', 'values', 'city', 'value']
 tags_for_Median_income = ['pages', 'page', 'tables', 'table', 'data', 'attribute', 'values', 'city', 'value']
-
-
-file = urllib2.urlopen(URL)
-data = file.read()
-file.close()
-dom = parseString(data)
-#retrieve the first xml tag (<tag>data</tag>) that the parser finds with name tagName:
-xmlTag = dom.getElementsByTagName('response')[0].toxml()
-#strip off the tag (<tag>data</tag>  --->   data):
-xmlData=xmlTag.replace('<response>','').replace('</response>','')
-
-# Retrieve the tag for median home value
-Median_Home_Val = original_dom(data)
-for tag in tags_for_Median_Home_val:
-	Median_Home_Val = dom.getElementsByTagName(tag)[0].toxml()
-Median_Home_Val= int(Median_Home_Val.replace('<value type="USD">', '').replace('</value>', ''))
-
-Median_income = parseString(data)
-count = 0
-for tag in tags_for_Median_income:
-	if tag == 'page':
-		Median_income = Median_income.getElementsByTagName(tag)[2].toxml()
-		Median_income = parseString(Median_income)
-	else:
-		Median_income = Median_income.getElementsByTagName(tag)[0].toxml()
-		if tag != "value":
+tags_for_single_male = ['pages', 'page']
+def get_median_income(data):
+	Median_income = parseString(data)
+	for tag in tags_for_Median_income:
+		if tag == 'page':
+			Median_income = Median_income.getElementsByTagName(tag)[2].toxml()
 			Median_income = parseString(Median_income)
+		else:
+			Median_income = Median_income.getElementsByTagName(tag)[0].toxml()
+			if tag != "value":
+				Median_income = parseString(Median_income)
+	Median_income = str( Median_income.replace('<value currency="USD">', '').replace('</value>', '') )
+	return Median_income
 
-		
-Median_income = int( Median_income.replace('<value currency="USD">', '').replace('</value>', '') )
-print Median_income
+def get_med_houseVal(data):
+	dom = parseString(data)
+	for tag in tags_for_Median_Home_val:
+		Median_Home_Val = dom.getElementsByTagName(tag)[0].toxml()
+	Median_Home_Val= str(Median_Home_Val.replace('<value type="USD">', '').replace('</value>', ''))
+	return Median_Home_Val
+
+def get_api_responses(urls):
+	api_responses = []
+	for url in urls:
+		file = urllib2.urlopen(url)
+		data = file.read()
+		file.close()
+		api_responses.append(data)
+	return api_responses
+
+def get_single_male(data):
+	fo = open("foo.xml","w+")
+	fo.write(data)
+	fo.close()
+
+	tree = ET.parse("foo.xml")
+	root = tree.getroot()
+	root = root[2]
+	root = root[4]
+	root = root[2]
+	root = root[1]
+	root = root[0]
+	root = root [1]
+	root = root[1]
+	root = root[1] 
+	return root[0][0].text
+
+def get_single_female(data):
+	fo = open("foo.xml","w+")
+	fo.write(data)
+	fo.close()
+
+	tree = ET.parse("foo.xml")
+	root = tree.getroot()
+	root = root[2]
+	root = root[4]
+	root = root[2]
+	root = root[1]
+	root = root[0]
+	root = root [1]
+	root = root[2]
+	root = root[1] 
+	return root[0][0].text
+
+def get_rent_percent(data):
+	fo = open("foo.xml","w+")
+	fo.write(data)
+	fo.close()
+
+	tree = ET.parse("foo.xml")
+	root = tree.getroot()
+	root = root[2][4][1][1][0][1][1][1][0][0]
+	# This would get me national avg root = root[2][4][1][1][0][1][1][1][1][0]
+	return root.text
+
+def get_homes_wKids(data):
+	fo = open("foo.xml","w+")
+	fo.write(data)
+	fo.close()
+
+	tree = ET.parse("foo.xml")
+	root = tree.getroot()
+	root = root[2][4][2][1][0][1][4][1][0][0]
+	return root.text
+
+api_responses = get_api_responses(URLS)
+
+cnx = mysql.connector.connect(user='root', database=DB_NAME)
+cursor = cnx.cursor()
+
+add_city = ("INSERT INTO zillow (cityName, singleMale, singleFemale, homePrice, singleHomes, rentPercent, avgIncome) VALUES (%s, %s, %s, %s, %s, %s, %s)")
+for name, data in zip(CITIES, api_responses):
+	sMale = get_single_male(data)
+	sFemale = get_single_female(data)
+	homeP = get_med_houseVal(data)
+	sHomes = get_homes_wKids(data)
+	rent = get_rent_percent(data)
+	medIncome = get_median_income(data)
+	city_data = (name, sMale, sFemale, homeP, sHomes, rent, medIncome)
+	cursor.execute(add_city, city_data)
+	cnx.commit()
+cursor.close()
+cnx.close()
+
 
